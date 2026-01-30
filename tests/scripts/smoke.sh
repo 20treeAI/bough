2f93c299cc6f72bf201ba9425bcaf82be8659b7d
Andrew Nichols <andrew@frenata.net>
2026-01-30 11:52:02 -0500

    fix smoke test

--- tests/scripts/smoke.sh
+++ tests/scripts/smoke.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 set -e
 
-bough graph --workspace tests/fixtures/sample-workspace
-bough analyze --workspace tests/fixtures/sample-workspace
+bough graph --workspace tests/fixtures/sample-workspace --repo tests/fixtures/sample-workspace
+bough analyze --workspace tests/fixtures/sample-workspace --repo tests/fixtures/sample-workspace
